e77b079432185686939aad2f74f0ad0efac252e7
Adarsh Tripathi <iec2019024@iiita.ac.in>
2020-06-30 00:04:35 +0530

    added index page and did some styling

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.use(express.static(__dirname + '/public'));
 app.get("/results", function(req, res){
     request("https://api.covid19api.com/summary", function(error, data){
         if(!error && response.statusCode == 200){
-            var myObj = JSON.parse(data);
-            res.render("results", {data: myObj});
+            var data = JSON.parse(data["body"]);
+            res.render("results", {data: data});
         }
     });
 });
 
-app.get("/india", function(req, res){
+app.get("/indiaStats", function(req, res){
     request("https://api.covid19india.org/data.json", function(error, data){
         if(!error && response.statusCode == 200){
-            res.render("india", {data: JSON.parse(data["body"])});
+            res.render("indiaStats", {data: JSON.parse(data["body"])});
         }
     });
 });
