3e8be8c753f9d85bc8ee82368094bc72ad6edb5a
Adarsh Tripathi <iec2019024@iiita.ac.in>
2020-07-17 00:17:39 +0530

    added pie chart

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import Dailydeceased from './charts/dailyDeath';
 import Dailyrecovered from './charts/dailyRecovered';
 import Totaldeceased from './charts/totalDeath';
 import Totalrecovered from './charts/totalRecovered';
+import PieTotal from './charts/pieTotal';
 
 class Home extends Component {
     state= {
@@ -26,28 +27,33 @@ class Home extends Component {
           <div>
             <div id="top-content">
               <table id="top-table">
-                <thead>
-                  <tr>
-                    <th id="c">Confirmed</th>
-                    <th id="a">Active</th>
-                    <th id="r">Recoveries</th>
-                    <th id="d">Deaths</th>
-                  </tr>
-                </thead>
                 {posts.map(post => {
                   if (post.statecode === "TT")
                     return (
                       <tbody key={post.statecode}>
                         <tr>
-                          <td id="c">{post.confirmed}</td>
-                          <td id="a">{post.active}</td>
-                          <td id="r">{post.recovered}</td>
-                          <td id="d">{post.deaths}</td>
+                          <td id="c">
+                            <h2>{post.confirmed}</h2>
+                            <p>Confirmed</p>
+                          </td>
+                          <td id="a">
+                            <h2>{post.active}</h2>
+                            <p>Active</p>
+                          </td>
+                          <td id="r">
+                            <h2>{post.recovered}</h2>
+                            <p>Recovered</p>
+                          </td>
+                          <td id="d">
+                            <h2>{post.deaths}</h2>
+                            <p>Deaths</p>
+                          </td>
                         </tr>
                       </tbody>
                     )
                 })}
               </table>
+              <PieTotal />
             </div>
             
             <section>
---
+++ src/components/charts/pieTotal.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Pie } from 'react-chartjs-2';
+import axios from 'axios';
+
+class PieTotal extends Component {
+    state = {
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: "Points",
+                    backgroundColor: ['#f1c40f', '#e67e22', '#16a085', '#2980b9'],
+                    data: []
+                }
+            ]
+        }
+    }
+    componentDidMount() {
+        axios.get("https://api.covid19india.org/data.json")
+            .then(res => {
+                console.log(res.data);
+                const dataSet = res.data.statewise[0];
+                console.log(dataSet);
+                this.setState({
+                    data: {
+                        labels: [],
+                        datasets: [
+                            {
+                                label: "Points",
+                                backgroundColor: ['green', 'rgb(36, 37, 128)','rgb(66, 135, 245)','red'],
+                                data: [parseInt(dataSet.confirmed),parseInt(dataSet.active), parseInt(dataSet.recovered), parseInt(dataSet.deaths) ] 
+                            }
+                        ]
+                    }
+                });
+            });
+    }
+    render() {
+        return (
+            <div style={{ position: "relative"}}>
+                <Pie
+                    options={{
+                        responsive: true
+                    }}
+                    data={this.state.data}
+                />
+            </div>
+        )
+    }
+}
+
+export default PieTotal
