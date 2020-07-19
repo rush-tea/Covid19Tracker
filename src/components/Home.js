be6c6461f69d20923dc5674e824c1fd2a5c6fdb5
Adarsh Tripathi <iec2019024@iiita.ac.in>
2020-07-20 03:03:12 +0530

    basic styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 import StateData from './stats/stateData';
-import TotalConfirmed from './charts/lineCharts/totalConfirmed';
-import Dailydeceased from './charts/lineCharts/dailyDeath';
-import Totaldeceased from './charts/lineCharts/totalDeath';
-import Totalrecovered from './charts/lineCharts/totalRecovered';
 import TotalStats from "./stats/totalStats";
 
 class Home extends Component {
@@ -14,13 +10,6 @@ class Home extends Component {
             <section>
               <div>
                 <StateData />
-              </div>
-              <div id="graphs" className="charts">
-                <div></div>
-                <div><TotalConfirmed /></div>
-                <div><Totalrecovered /></div>
-                <div><Dailydeceased /></div>
-                <div><Totaldeceased /></div>
               </div>
             </section>
           </div>
