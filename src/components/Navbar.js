0d098c4cbdb751ff524dd37c253319b9a4ea83ba
Adarsh Tripathi <iec2019024@iiita.ac.in>
2020-07-22 06:56:29 +0530

    done styling mainpage maincontent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,16 +23,19 @@ class Nav extends Component {
             <nav>
                 <div className="logo-name">
                     <p>Covid19India</p>
+                    <span>Stats overview</span>
                 </div>
                 <i
-                    className="fa fa-bars hamburger"
+                    className={` fa fa-bars hamburger ${isExpanded ? "clicked" : ""}`}
                     aria-hidden="true"
                     onClick={e => this.handleToggle(e)}
                 />
                 <ul className={`nav-links ${isExpanded ? "open" : ""}`}>
-                    <li>home</li>
-                    <li>about</li>
-                    <li>contact</li>
+                        <li> <Link to="/">Home</Link> </li>
+                        <li><a href="https://www.covid19india.org/">Deep Dive</a></li>
+                        <li> <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public"> WHO Guidelines </a></li>
+                        <li> <a href="https://github.com/covid19india/api">View Database</a></li>
+                        <li> <a href="#contacts">Contact Us</a> </li>
                 </ul>
             </nav>
             <hr/>
